58dd4a2bc7c643bcd83c11c60352e01154298401
Mitchsbay <mitchsbay@gmail.com>
2025-08-25 15:37:20 +1000

    Update firstrow.ts

--- src/lib/firstrow.ts
+++ src/lib/firstrow.ts
@@ -1,3 +1,3 @@
 export function firstRow<T>(data: T[] | null | undefined): T | null {
-  return (Array.isArray(data) && data.length > 0) ? data[0] : null;
+  return Array.isArray(data) && data.length > 0 ? data[0] : null;
 }
