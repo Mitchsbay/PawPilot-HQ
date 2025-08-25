867203e40e714ba747a366c98f80f113a31e5d2c
Mitchsbay <mitchsbay@gmail.com>
2025-08-25 15:38:30 +1000

    Update flags.ts

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -1,7 +1,6 @@
 import { supabase } from "./supabase";
 import { getFeatureFlagSafe, getAllFeatureFlagsSafe } from "./safeSupabase";
-import { firstRow } from "./firstRow";
-import { firstRow } from "./firstRow";
+import { firstRow } from "@/lib/firstRow";
 
 const cache = new Map<string, boolean>();
 
